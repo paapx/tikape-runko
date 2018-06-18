5fdb567d62309040321a89539fa52792a3d7140a
paapx <paupaavil@gmail.com>
2018-06-19 01:35:55 +0300

    viesti

--- src/main/java/tikape/reseptiarkisto/Main.java
+++ src/main/java/tikape/reseptiarkisto/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
+            map.put("viesti", ":");
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
--- src/main/java/tikape/reseptiarkisto/database/Database.java
+++ src/main/java/tikape/reseptiarkisto/database/Database.java
@@ -19,35 +19,4 @@ public class Database {
         
         return DriverManager.getConnection("jdbc:sqlite:reseptiarkisto.db");
     }
-
-    public void init() {
-        List<String> lauseet = sqliteLauseet();
-
-        // "try with resources" sulkee resurssin automaattisesti lopuksi
-        try (Connection conn = getConnection()) {
-            Statement st = conn.createStatement();
-
-            // suoritetaan komennot
-            for (String lause : lauseet) {
-                System.out.println("Running command >> " + lause);
-                st.executeUpdate(lause);
-            }
-
-        } catch (Throwable t) {
-            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
-            System.out.println("Error >> " + t.getMessage());
-        }
-    }
-
-    private List<String> sqliteLauseet() {
-        ArrayList<String> lista = new ArrayList<>();
-
-        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Annos (id integer PRIMARY KEY, nimi varchar(255));");
-        lista.add("INSERT INTO Annos (nimi) VALUES ('Annos1');");
-        lista.add("INSERT INTO Annos (nimi) VALUES ('Annos2');");
-        lista.add("INSERT INTO Annos (nimi) VALUES ('Annos3');");
-
-        return lista;
-    }
 }
